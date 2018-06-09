49416e27ae7925febbff489d7b911f3265571ab6
Jordan Garrison <jordan.andrew.garrison@gmail.com>
2018-06-08 19:21:46 -0500

    Pulls microsoft stocks down and prints them now

--- python/stock_stalking.py
+++ python/stock_stalking.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import requests
-import yaml
 import logging
+import configparser
+import sys
 
 class StockStalking:
     """
@@ -13,15 +14,42 @@ class StockStalking:
         """
         Intantiate the class
         """
-        self.url = "www.github.com"
+        config = configparser.ConfigParser()
+        config.read('stock_stalking.ini')
+        url = config['General']['url']
+        self.api_key = config['General']['api_key']
+        self.logfile = config['General']['logfile']
+        # https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo
+        self.url = url + '/query?function=' + config['Data']['function'] + '&symbol=' + config['Data']['symbol'] + '&apikey=' + self.api_key 
+
+    def get_data(self, url=""):
+        """
+        Function which takes the full url and returns the data dictionary.
+        Utilizes the requests package.
+        You can either pass in a URL or use one predefined in the config file.
+        """
+        # Check if variable contains anything
+        try:
+            if url:
+                r = requests.get(url)
+            else:
+                r = requests.get(self.url)
+        except:
+                logging.error("No URL specified")
+                sys.exit(2)  
+
+        # Exctract data and return dict
+        return r.json()
+        
 
 def main():
     ss = StockStalking()
-    try:
-        r = requests.get("https://www.yelp.com")
-        print(r.content)
-    except:
-        print("Error on request")
+    print(ss.url)
+    data = ss.get_data()
+    print(data)
+    for item in data['Time Series (Daily)']:
+        print(item)
+        print(data['Time Series (Daily)'][item])
 
 if __name__ == '__main__':
     main()
